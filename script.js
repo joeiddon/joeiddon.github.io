32c186c310208ec76afbdd0ade6f0409f2a26847
joe iddon <joedominiciddon@gmail.com>
2017-07-23 22:22:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,42 +3,33 @@ for (var i=0;i<nav_buttons.length;i++){
 	nav_buttons[i].onclick = function(){
 		window.location.href=this.getAttribute("href");
 	}
-	nav_buttons[i].onmouseover = function(){
-		this.style.backgroundColor = "#20B7BF";
-		//this.style.color="#00979F";
-	}
-	nav_buttons[i].onmouseout = function(){
-		this.style.backgroundColor = "";
-		//this.style.color="";
-	}
 }
 
 logo = document.getElementById("logo");
 logo.onclick = function(){
 	window.location.href=this.getAttribute("href");
 }
-logo.onmouseover = function(){
-	this.style.backgroundColor = "#20B7BF";
-	//this.style.color="#00979F";
-	//document.getElementById("logo_img").src = "https://raw.githubusercontent.com/RoadKillCat/roadkillcat.github.io/master/images/logo_teal.png"
-}
-logo.onmouseout = function(){
-	this.style.backgroundColor = "";
-	//this.style.color="";
-	//document.getElementById("logo_img").src = "https://raw.githubusercontent.com/RoadKillCat/roadkillcat.github.io/master/images/logo_white.png"
-}
-
 logo_links = document.getElementsByClassName("logo_link");
 for (var i=0;i<logo_links.length;i++){
 	logo_links[i].onclick = function(){
 		window.location.href=this.getAttribute("href");
 	}
-	logo_links[i].onmouseover = function(){
-		this.style.backgroundColor = "#20B7BF";
-		//this.style.color="#00979F";
+}
+
+projects = document.getElementById("projects")
+dropdown = document.getElementById("dropdown")
+projects.onmouseover = function(){
+	dropdown.style.display = "block"
+}
+
+projects.onmouseout = function(e){
+	if (e.relatedTarget.parentNode != dropdown){
+		dropdown.style.display = "none"
 	}
-	logo_links[i].onmouseout = function(){
-		this.style.backgroundColor = "";
-		//this.style.color="";
+}
+
+dropdown.onmouseout = function(e){
+	if (e.relatedTarget.parentNode != this || e.relatedTarget != this){
+		this.style.display = "none"
 	}
 }
