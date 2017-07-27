80f858609af4c6d98713c1c7f5d3398a04499a68
joe iddon <joedominiciddon@gmail.com>
2017-07-27 11:38:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,31 +23,31 @@ for (var i=0;i<logo_links.length;i++){
 	}
 }
 
-projects = document.getElementById("projects")
-dropdown = document.getElementById("dropdown")
+projects = document.getElementById("projects");
+dropdown = document.getElementById("dropdown");
 
 if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 	projects.onmouseover = function(){
-		dropdown.style.display = "block"
+		dropdown.style.display = "block";
 	}
 
 	projects.onmouseout = function(e){
 		if (e.relatedTarget.parentNode != dropdown){
-			dropdown.style.display = "none"
+			dropdown.style.display = "none";
 		}
 	}
 
 	dropdown.onmouseout = function(e){
 		if (e.relatedTarget.parentNode != this && e.relatedTarget != this){
-			this.style.display = "none"
+			this.style.display = "none";
 		}
 	}
 } else {
-	projets.onclick = function(){
+	projects.onclick = function(){
 		if (dropdown.style.display="block"){
-			window.location.href = this.getAttribute("href")
-		else {
-			dropdown.style.display = "block"
+			window.location.href = this.getAttribute("href");
+		} else {
+			dropdown.style.display = "block";
 		}
 	}
 }
