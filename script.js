047196588572adb2fff32b3cbc09875be54ddc0c
joeiddon <joedominciddon@gmail.com>
2018-07-01 21:35:25 +0100

    neatened html, re-styled url-message

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-projects = document.getElementById('projects');
-dropdown = document.getElementById('dropdown');
+let projects = document.getElementById('projects');
+let dropdown = document.getElementById('dropdown');
 
 //if on phone, requires click instead of mouse hover
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if (/android|webos|iphone|ipad|ipod|blackberry/i.test(navigator.userAgent) ) {
     projects.onclick = function(){
         if (dropdown.style.display == 'block'){
             window.location.href = this.getAttribute('href');
@@ -20,5 +20,6 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
         }
     }
 }
+
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-103594964-1', 'auto');ga('send', 'pageview');
