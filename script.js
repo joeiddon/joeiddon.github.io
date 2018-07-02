deec2242dfad77f6fd4c19a8acdfb4538a9a1ca4
joeiddon <joedominciddon@gmail.com>
2018-07-02 19:02:49 +0100

    padding mucked up height so fixed

--- script.js
+++ script.js
@@ -34,5 +34,13 @@ for (let i = 0; i < iframes.length; i++){
     iframes[i].height = main_container.clientWidth/16*9;
 }
 
+//resize any <script> elements to be the size of the main_container
+let scripts = document.getELementsByTagName('script');
+for (let i = 0; i < scripts.length; i++){
+    scripts[i].width  = main_container.clientWidth;
+    scripts[i].height = main_container.clientWidth/16*9;
+}
+
+
 //google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-103594964-1', 'auto');ga('send', 'pageview');
