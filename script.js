e1fa391ae6333d8c0fb010d301170335ba434917
joeiddon <joedominiciddon@gmail.com>
2019-01-09 21:55:03 +0000

    joe's analytics

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ for (let i = 0; i < scripts.length; i++){
 }
 */
 
+//joe's analytics :)
+let xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://joe.iddon.com:8765');
+xhr.send(window.location.href);
 
 //google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-103594964-1', 'auto');ga('send', 'pageview');
